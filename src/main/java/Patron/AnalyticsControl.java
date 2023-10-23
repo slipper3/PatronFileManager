7d683dfbe1375f09a9fe4687b37776ed72ff5b37
Mahovik <amahovik28@gmail.com>
2023-10-23 18:39:22 +0300

    finished ui design 1.0

--- src/main/java/Patron/AnalyticsControl.java
+++ src/main/java/Patron/AnalyticsControl.java
@@ -1,9 +1,14 @@
 package Patron;
 
+import Moduls.TableViewItem;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
@@ -14,11 +19,33 @@ public class AnalyticsControl {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    public TableView<TableViewItem> AnalyticTable;
+    @FXML
+    public VBox AmalyticVBox;
+    @FXML
+    public Button SwitchScene;
+    @FXML
+    public Button Back;
+
+
     public void switchToMainScene(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ManagerUi.fxml")));
         stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    public void back(javafx.event.ActionEvent actionEvent) throws IOException {
+        SwitchScene.setVisible(true);
+        AmalyticVBox.setVisible(true);
+        Back.setVisible(false);
+        AnalyticTable.setVisible(false);
+    }
+    public void findFile(javafx.event.ActionEvent actionEvent) throws  IOException {
+        Back.setVisible(true);
+        AnalyticTable.setVisible(true);
+        SwitchScene.setVisible(false);
+        AmalyticVBox.setVisible(false);
     }
 }
--- src/main/java/Patron/Main.java
+++ src/main/java/Patron/Main.java
@@ -18,7 +18,5 @@ public class Main extends Application {
         stage.setScene(new Scene(root));
         stage.show();
     }
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) {launch(args);}
 }
